bffb603fc5b58cc5cec66bb4395b335113a8f43a
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-03-14 17:43:23 -0600

    Login, registro y envío de otp finalizado

--- components/login/Form.tsx
+++ components/login/Form.tsx
@@ -1,13 +1,26 @@
 'use client';
 
 import Link from "next/link";
-import { useState } from "react";
+import { redirect, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import OtpCode from "../pop-ups/OtpCode";
 
 const FormLogin = () => {
 
+    const router = useRouter();
+
     const [view, setView] = useState<boolean>(false);
     const [accept, setAccept] = useState<boolean>(false);
-    const [error, setError] = useState<Set<string>>(new Set());
+    const [error, setError] = useState<string[]>([]);
+
+
+    // * Comprueba si la sesión ya está iniciada.
+    useEffect(()=>{
+        const storageData = localStorage.getItem('SessionData');
+        const data = storageData ? JSON.parse(storageData) : null;
+
+        if (data) redirect(`/mi-perfil/${btoa(data.Token)}`);
+    },[]);
 
     // * Email
     const [email, setEmail] = useState<string>('');
@@ -18,86 +31,173 @@ const FormLogin = () => {
 
         if (!emailRegex.test(email)) {
 
-            if (!error.has(errorMessage)) {
-                // const updateError = new Set([...error, errorMessage]);
-                // setError(updateError);
-            }
+            const repeat = error.find(message => message === errorMessage);
+
+            if (repeat) return;
+
+            setError([...error, errorMessage]);
         }else{
-            // const updateError = error.filter(message => message != 'Introduce una dirección de correo válida');
-            // setError(updateError);
+            const updateError = error.filter(message => message != errorMessage);
+            setError(updateError);
         }
     }
 
+    // * Password
+    const [password, setPassword] = useState<string>('');
+
+    const onBlurPassword = () => {
+        const errorMessage = 'La contraseña debe tener al menos 8 caracteres, incluyendo letras y números.';
+        const regex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
+        if (!regex.test(password)) {
+            const repeat = error.find(message => message === errorMessage);
+
+            if (repeat) return;
+
+            setError([...error, errorMessage]);
+        }else{
+            const updateError = error.filter(message => message != errorMessage);
+            setError(updateError);
+        }
+    }
+
+    // * Submit formulario
+    const [notSubmit, setNotSubmit] = useState<string>('');
+    
+    const onSubmitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (error.length > 0) {
+            setNotSubmit('Falta uno o más campos por llenar');
+            return; 
+        }
+
+        const response = await fetch('https://souvenir-site.com/WebTarjet/APIUsuDtos/Login',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: JSON.stringify({
+                "Cuenta": email,
+                "Password": password,
+            })
+        });
+
+        const data = await response.json();
+
+        if (!data.Acceso) {
+            setNotSubmit('Correo o contraseña incorrectos');
+            return;
+        }
+
+        setNotSubmit('');
+        localStorage.setItem('SessionData', JSON.stringify(data));
+        router.push(`/${btoa(data.Token)}`);
+    }
+
+    // * Registrar Usuario
+    const [otpSend, setOtpSend] = useState<boolean>(false);
+
+    const registerUser = async () => {
+        if (error.length > 0) {
+            setNotSubmit('Falta uno o más campos por llenar');
+            return;
+        }
+
+        setNotSubmit('');
+        setOtpSend(true);
+    }
 
     return ( 
-        <form>
-            <input 
-                type="email" 
-                placeholder="Correo electrónico"
-                value={email}
-                onChange={(e)=>setEmail(e.target.value.trim())}
-                onBlur={onBlurEmail}
-            />
-
-            <div>
-                <input type={view ? 'text' : 'password'} placeholder="Escribe tu contraseña"/>
-
-                <button type="button" onClick={()=>setView(!view)}>
-                    {view ? <img src="/images/icono-ojo-cerrado.svg" alt="icono" /> 
-                    : <img src="/images/icono-ojo.svg" alt="icono" />}
-                </button>
-            </div>
-
-            <button className="link">
-                ¿Olvidaste tu contraseña?
-            </button>
-
-            { error &&
-                <div className="error">
-                    {/* { error.map((e)=>(
-                        <p key={e}>{e}</p>
-                    ))} */}
-                </div>
+        <>
+            {otpSend && 
+                <OtpCode email={email} password={password} close={() => setOtpSend(false)}/>
             }
 
-            <hr style={{borderColor: '#000', marginBottom: '16px'}}/>
-
-            <div className="check" style={{width: '50%', margin: 'auto'}}>
+            <form onSubmit={onSubmitForm}>
                 <input 
-                    type="checkbox" 
-                    id="accept" 
-                    checked={accept} 
-                    onChange={(e)=>setAccept(e.target.checked)}
+                    type="email" 
+                    placeholder="Correo electrónico"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value.trim())}
+                    onBlur={onBlurEmail}
                 />
 
-                <label htmlFor="accept">
-                    Acepto las condiciones de uso y la <Link href={'/aviso-de-privacidad'}>política de privacidad</Link>
-                </label>
-            </div>
+                <div>
+                    <input 
+                        type={view ? 'text' : 'password'} 
+                        placeholder="Escribe tu contraseña"
+                        value={password}
+                        onChange={(e)=>setPassword(e.target.value.trim())}
+                        onBlur={onBlurPassword}
+                    />
 
-            <div style={{display: 'flex', justifyContent: 'center'}}>
-
-                <div style={{width: '35%'}}>
-                    <button 
-                        className="btn" 
-                        style={{background: '#fdc40a'}}
-                        disabled={!accept}
-                    >
-                        Registrar
+                    <button type="button" onClick={()=>setView(!view)}>
+                        {view ? <img src="/images/icono-ojo-cerrado.svg" alt="icono" /> 
+                        : <img src="/images/icono-ojo.svg" alt="icono" />}
                     </button>
                 </div>
 
-                <div style={{width: '35%', borderLeft: '1px solid #000'}}>
-                    <button 
-                        className="btn"
-                        disabled={!accept}
-                    >
-                        Iniciar sesión
-                    </button>
+                <button className="link">
+                    ¿Olvidaste tu contraseña?
+                </button>
+
+                { error &&
+                    <div className="error">
+                        { error.map((e)=>(
+                            <p key={e}>{e}</p>
+                        ))}
+                    </div>
+                }
+
+                <hr style={{borderColor: '#000', marginBottom: '16px'}}/>
+
+                <div className="check">
+                    <input 
+                        type="checkbox" 
+                        id="accept" 
+                        checked={accept} 
+                        onChange={(e)=>setAccept(e.target.checked)}
+                    />
+
+                    <label htmlFor="accept">
+                        Acepto las condiciones de uso y la <Link href={'/aviso-de-privacidad'}>política de privacidad</Link>
+                    </label>
                 </div>
 
-            </div>
-        </form>
+                { notSubmit &&
+                    <div className="error">
+                        <p>{notSubmit}</p>
+                    </div>
+                }
+
+                <div className="twoButtons">
+
+                    <div>
+                        <button 
+                            className="btn" 
+                            style={{background: '#fdc40a'}}
+                            disabled={!accept}
+                            type="button"
+                            onClick={registerUser}
+                        >
+                            Registrar
+                        </button>
+                    </div>
+
+                    <div style={{ borderLeft: '1px solid #000'}}>
+                        <button 
+                            className="btn"
+                            disabled={!accept}
+                            type="submit"
+                        >
+                            Iniciar sesión
+                        </button>
+                    </div>
+
+                </div>
+            </form>
+        </>
     );
 }
 
---
+++ components/pop-ups/OtpCode.tsx
@@ -0,0 +1,131 @@
+'use client';
+
+import { AnimatePresence, motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import OTPInput from "react-otp-input";
+import style from './otp.module.scss';
+import { useRouter } from "next/navigation";
+
+interface Props {
+    email:string;
+    password: string;
+    close: () => void;
+}
+
+const animate = {
+    initial: {scale: 0},
+    animate: {scale: 1},
+    transition: {delay: 1},
+    exit: {scale: 0}
+}
+
+
+const OtpCode = ( { email, password, close }:Props ) => {
+
+    const router = useRouter();
+    
+    useEffect(()=>{
+
+        const codeEmail = async () => {
+            await fetch(`https://souvenir-site.com/WebTarjet/APIUsuDtos/EnviarCodigoOTP?Nombre=&Email=${email}`, {
+                method: 'GET',
+                mode: 'cors'
+            });
+        }
+
+        codeEmail();
+
+    },[]);
+    
+    const [code, setCode] = useState('');
+    const [codeSend, setCodeSend] = useState<boolean>(false);
+    const [success, setSuccess] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+
+    const checkCode = async () => {
+
+        const response = await fetch('https://souvenir-site.com/WebTarjet/APIUsuDtos/ValidarCodigoOTP',{
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                "RegistroUsu": {
+                    "Nombre": "",
+                    "ApellidoPat": "",
+                    "ApellidoMat": "",
+                    "Codigo": code,
+                    "Correo": email,
+                    "Password": password
+                }
+            })
+        });
+
+        const data = await response.json();
+
+        if (data.Token) {
+            setCodeSend(true);
+
+            setTimeout(()=>{
+                setSuccess(true);
+
+                setTimeout(()=>{
+                    router.push(`/mi-perfil/${btoa(data.Token)}`);
+                },3000);
+            },3500)
+        }
+
+        setError('Código no válido, segúrese de ingresar el código exacto que le fue proporcionado. Si está teniendo dificultades, no dude en solicitar un nuevo código');
+    }
+
+    return ( 
+        <div className="pop">
+            <AnimatePresence>
+                { !codeSend && (
+                    <motion.div className={`container ${style.OTP}`} {...animate}>
+                        <h5>Verifica tu correo</h5>
+
+                        <OTPInput
+                            value={code}
+                            onChange={(otp:string)=>setCode(otp)}
+                            numInputs={6}
+                            renderInput={(props) => <input {...props} />}
+                        />
+
+                        <p>
+                            Hemos generado un código para usted y lo hemos enviado a su dirección de correo electrónico. Por favor, revise su bandeja de entrada. Si no encuentra el correo en la bandeja principal, le recomendamos verificar la carpeta de correo no deseado.
+                        </p>
+
+                        <p>¿No te llegó el código? <span>Enviar nuevamente</span></p>
+
+                        {error &&
+                            <p className="error">
+                                {error}
+                            </p>
+                        }
+
+                        <button 
+                            className="btn" 
+                            disabled={code.length < 6 ? true : false}
+                            onClick={checkCode}
+                        >
+                            Verificar código
+                        </button>
+
+                        <button className="close" onClick={close}>
+                            cerrar ventana (x)
+                        </button>
+                    </motion.div>
+                )}
+
+                { success && (
+                    <motion.div className={`container ${style.Success}`} {...animate}>
+                        <img src="/images/registro-exitoso.png" alt="Ilustración de registro exitoso" />
+                        <h5>Código proporcionado válido</h5>
+                        <p>Perfil creado con éxito</p>
+                    </motion.div>
+                )}
+            </AnimatePresence>
+        </div>
+    );
+}
+
+export default OtpCode;
