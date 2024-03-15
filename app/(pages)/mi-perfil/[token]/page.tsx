665654f4a59c74b805d7a4f16ee1df104e92536b
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-03-15 17:37:19 -0600

    Mi perfil sin fetch

---
+++ app/(pages)/mi-perfil/[token]/page.tsx
@@ -0,0 +1,22 @@
+import userData from "@/apis/userData";
+
+const MiPerfil = async () => {
+
+    const data = await userData();
+
+    console.log(data);
+
+    return ( 
+        <div className="green">
+            <div className="background">
+                <div className="body">
+                    <div className="contain">
+                        <h1>Hola mundo</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default MiPerfil;
--- components/login/Apple.tsx
+++ components/login/Apple.tsx
@@ -1,9 +1,61 @@
 'use client';
 
 import AppleLogin from 'react-apple-login';
+import { jwtDecode } from 'jwt-decode';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 const Apple = () => {
+
+    const router = useRouter();
+
+    const appleLogin = async ( response:any ) => {
+
+        const idToken = await response.authorization.id_token;
+        const decodedToken = jwtDecode(idToken);
+
+        const responseCode = await fetch('https://souvenir-site.com/WebTarjet/APIUsuDtos/ValidarCodigoOTP', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                "RegistroUsu": {
+                    "Nombre": response.user?.name.firstName ? response.user.name.firstName : '',
+                    "ApellidoPat": response.user?.name.lastName ? response.user.name.lastName : '',
+                    "ApellidoMat": "",
+                    "Codigo": decodedToken.sub,
+                    "Correo": response.user?.email ? response.user.email : '',
+                    "Password": decodedToken.sub,
+                    "TipoAut": "A"
+                }
+            })
+        });
+
+        const data = await responseCode.json();
+
+        if (data.Token) {
+            localStorage.setItem('SessionData', JSON.stringify(data));
+
+            router.push(`/${btoa(data.Token)}`);
+
+            return;
+        }
+
+        const responseLogin = await fetch('https://souvenir-site.com/WebTarjet/APIUsuDtos/Login', {
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify({
+                "Cuenta": "",
+                "Password": decodedToken.sub,
+                "tipoLogin": "A"
+            })
+        });
+
+        const dataLogin = await responseLogin.json();
+
+        localStorage.setItem('SessionData', JSON.stringify(dataLogin));
+        router.push(`/${btoa(dataLogin.Token)}`);
+    }
+
     return ( 
         <AppleLogin 
             clientId="site.tarjet.client"
@@ -12,11 +64,11 @@ const Apple = () => {
             scope = "name email"
             responseType={"code"} 
             responseMode={"query"}  
-            // callback={HandleAppleLogin}
-            // onError={(error) => console.error(error)}
-            // usePopup={true}
+            callback={appleLogin}
+            // onError={(error:AppleLoginProps) => console.error(error)}
+            usePopup={true}
             render={(props) => (
-                <button >
+                <button onClick={props.onClick} disabled={props.disabled}>
                     <Image 
                         src={'/images/icono-apple.svg'}
                         alt='icono apple'
