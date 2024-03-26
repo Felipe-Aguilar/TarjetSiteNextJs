cba770e28107411f4bf059118c185df9f8370690
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-03-26 13:50:45 -0600

    Directorio finalizado sin responsivo

--- api/searchByName.ts
+++ api/searchByName.ts
@@ -11,6 +11,14 @@ export default async function searchByName(name:string, latitude:any, longitude:
 
         const data = await response.json();
 
+        if (data.ListTarjets.length === 0) {
+            const response = await fetch(`https://souvenir-site.com/WebTarjet/APIDirectorio/BuscaXDesc?Actividad=&Nombre=&Alias=${name}&Latitud=${latitude}&Longitud=${longitude}&Radio=3`);
+
+            const data = await response.json();
+
+            return data;
+        }
+
         return data;
     }
 
--- components/directory/Search.tsx
+++ components/directory/Search.tsx
@@ -3,14 +3,16 @@
 import Slider from 'react-slick';
 import Image from 'next/image';
 import style from './search.module.scss';
-
-import { useEffect, useState } from 'react';
-import { BsSearch, BsXLg } from 'react-icons/bs';
-import { SDTCategoria } from '@/interfaces/categoriesDirectory-infertace';
-import { AnimatePresence, motion } from 'framer-motion';
-import { ResultUserInterface, ListTarjet } from '@/interfaces/resultUser-interface';
 import Link from 'next/link';
 import searchByName from '@/api/searchByName';
+
+import { useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { SDTCategoria } from '@/interfaces/categoriesDirectory-infertace';
+import { ResultUserInterface } from '@/interfaces/resultUser-interface';
+import { BsSearch, BsXLg } from 'react-icons/bs';
+import { json } from 'stream/consumers';
+import { OptionCategoriesInterface } from '@/interfaces/optionCategories-interface';
 
 type Props = {
     categories: {
@@ -99,6 +101,8 @@ const Search = ( {categories}:Props ) => {
 
     const onSearchName = async (name:string) => {
         setName(name);
+        setCategorySelected('');
+        setOptionsCategories(undefined);
 
         const data = await searchByName(name, position?.coords.latitude, position?.coords.longitude);
 
@@ -114,13 +118,45 @@ const Search = ( {categories}:Props ) => {
     const deleteName = () => {
         setSearch(false);
         setName('');
+        setCategorySelected('');
     }
 
     // * Selección de categoría
     const [categorySelected, setCategorySelected] = useState<string>('');
+    const [optionCategories, setOptionsCategories] = useState<OptionCategoriesInterface>();
 
-    const onClickCategory = (idCategory : string) => {
+    const onClickCategory = async (idCategory : string) => {
         setCategorySelected(idCategory);
+
+        setResults(undefined);
+        setName('');
+
+        const response = await fetch(`https://souvenir-site.com/WebTarjet/APIUsuDtos/ConsultaCategorias?Nivel1=${idCategory}&Nivel2=`);
+
+        const data = await response.json();
+
+        setOptionsCategories(data);
+
+        setSearch(true);
+    }
+
+    // *Búsqueda de usuarios por categoría
+    const onSearchCategory = async (idCategory:string) => {
+        
+        const response = await fetch(`https://souvenir-site.com/WebTarjet/APIDirectorio/ConsultaTarjetCategorias?Nivel1=&Nivel2=${idCategory}`);
+
+        const data = await response.json();
+
+        setResults(data);
+
+        setOptionsCategories(undefined);
+    }
+    
+    // * Cerrar resultados
+    const onCloseResults = () => {
+        setSearch(false);
+        setName('');
+        setCategorySelected('');
     }
 
     return ( 
@@ -183,54 +219,108 @@ const Search = ( {categories}:Props ) => {
             </div>
 
             { search && 
-                <AnimatePresence>
-                    {search &&
-                        <motion.div {...animation} className={style.ResultsSearch}>
-                            <button className={style.Close} onClick={()=>setSearch(false)}>
-                                <BsXLg />
-                                Cerrar ventana de resultados
-                            </button>
+                <motion.div {...animation} className={style.ResultsSearch}>
+                    <button className={style.Close} onClick={onCloseResults}>
+                        <BsXLg />
+                        Cerrar ventana de resultados
+                    </button>
 
-                            <div className={style.ResultContainer}>
-                                { results?.ListTarjets.map((user)=>(
-                                    <div key={user.IdUsuario} className='userResult'>
-                                        <div className='header'>
-                                            <Image 
-                                                src={
-                                                    user.ImgFoto 
-                                                    ? `https://tarjet.site/imagenes/perfil-imagenes/PERF_${user.Token}.webp`
-                                                    : '/images/perfil-temporal.webp'
-                                                }
-                                                alt='Foto de perfil'
-                                                width={200}
-                                                height={200}
-                                                priority={false}
-                                            />
-                                            <div className='text'>
-                                                <h5>{user.NombreCompleto}</h5>
-                                                <span>{user.Actividad}</span>
+                    { results &&
+                        <div className={style.ResultContainer}>
+                            { results?.ListTarjets.map((user)=>(
+                                user.PublicPriva === 0 && (
+                                    user.RegistroTarjet && (    
+                                        range ? parseFloat(user.Distancia) <= 3 && (
+                                            <div key={user.IdUsuario} className='userResult'>
+                                                <div className='header'>
+                                                    <Image 
+                                                        src={
+                                                            user.ImgFoto 
+                                                            ? `https://tarjet.site/imagenes/perfil-imagenes/PERF_${user.Token}.webp`
+                                                            : '/images/perfil-temporal.webp'
+                                                        }
+                                                        alt='Foto de perfil'
+                                                        width={200}
+                                                        height={200}
+                                                        priority={false}
+                                                    />
+                                                    <div className='text'>
+                                                        <h5>{user.NombreCompleto}</h5>
+                                                        <span>{user.Actividad}</span>
+                                                    </div>
+                                                </div>
+                                                <div className='card'>
+                                                    <Link href={`/st/${btoa(user.Token)}`}>
+                                                        <Image 
+                                                            src={`https://tarjet.site/imagenes/tarjetas_frente_usuarios/TFRE_${user.Token}.webp`}
+                                                            alt='Tarjeta de presentación'
+                                                            width={500}
+                                                            height={190}
+                                                            priority={false}
+                                                        />
+                                                    </Link>
+                                                </div>
+                                                <div className='footer'>
+                                                    <span>Da click sobre la imagen para ver tarjeta digital</span>
+                                                </div>
                                             </div>
-                                        </div>
-                                        <div className='card'>
-                                            <Link href={`/st/${btoa(user.Token)}`}>
+                                        )
+                                        :
+                                        <div key={user.IdUsuario} className='userResult'>
+                                            <div className='header'>
                                                 <Image 
-                                                    src={`https://tarjet.site/imagenes/tarjetas_frente_usuarios/TFRE_${user.Token}.webp`}
-                                                    alt='Tarjeta de presentación'
-                                                    width={500}
-                                                    height={190}
+                                                    src={
+                                                        user.ImgFoto 
+                                                        ? `https://tarjet.site/imagenes/perfil-imagenes/PERF_${user.Token}.webp`
+                                                        : '/images/perfil-temporal.webp'
+                                                    }
+                                                    alt='Foto de perfil'
+                                                    width={200}
+                                                    height={200}
                                                     priority={false}
                                                 />
-                                            </Link>
+                                                <div className='text'>
+                                                    <h5>{user.NombreCompleto}</h5>
+                                                    <span>{user.Actividad}</span>
+                                                </div>
+                                            </div>
+                                            <div className='card'>
+                                                <Link href={`/st/${btoa(user.Token)}`}>
+                                                    <Image 
+                                                        src={`https://tarjet.site/imagenes/tarjetas_frente_usuarios/TFRE_${user.Token}.webp`}
+                                                        alt='Tarjeta de presentación'
+                                                        width={500}
+                                                        height={190}
+                                                        priority={false}
+                                                    />
+                                                </Link>
+                                            </div>
+                                            <div className='footer'>
+                                                <span>Da click sobre la imagen para ver tarjeta digital</span>
+                                            </div>
                                         </div>
-                                        <div className='footer'>
-                                            <span>Da click sobre la imagen para ver tarjeta digital</span>
-                                        </div>
-                                    </div>
-                                ))}
-                            </div>
-                        </motion.div>
+                                    )
+                                )
+                            ))}
+                        </div>
                     }
-                </AnimatePresence>
+
+                    {results?.ListTarjets.length == 0 && <p className={style.NoResults}>No se encontraron resultados</p>}
+
+                    { optionCategories &&
+                        <>
+                            {optionCategories.SDTCategorias.map((level)=>(
+                                <div key={level.Id} className={style.optionCategories}>
+                                    {level.Level2.map((category)=>(
+                                        <button key={category.Id} onClick={()=>onSearchCategory(category.Id)}>
+                                            {category.Desc}
+                                        </button>
+                                    ))}
+                                </div>
+                            ))}
+                        </>
+                    }
+                </motion.div>
             }
         </div>
     );
