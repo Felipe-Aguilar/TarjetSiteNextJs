e60653eb48d6f5b7a8eed6d10e9e8788ad467947
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-04-01 16:09:40 -0600

    Solución a AuthJs 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,47 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const authOptions:NextAuthOptions = {
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                usuId: { label: "usuId", type: "text", placeholder: "usuId" },
-                token: { label: "token", type: "text", placeholder: "token" },
-            },
-            async authorize(credentials) {
-
-                if (credentials) {
-                    return {
-                        id: credentials.usuId,
-                        name: credentials.usuId,
-                        email: credentials.token,
-                    }
-                }
-
-                return null;
-            },
-        })
-    ],
-    session: {
-        strategy: 'jwt'
-    },
-    callbacks: {
-        async signIn({ user }){
-            return true;
-        },
-        async jwt({ token }){
-            return token;
-        },
-        async session({ session }){
-
-            return session;
-        }
-    },
-    // pages: {
-    //     signIn: '/login'
-    // }
-}
+import NextAuth from "next-auth";
+import { authOptions } from "../../lib/auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
