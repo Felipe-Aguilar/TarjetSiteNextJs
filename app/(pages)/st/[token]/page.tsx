b05b77d179f723905b5f97e77be9c4f65045abe3
Felipe Aguilar <felipe.aguilar0g@gmail.com>
2024-05-13 12:58:13 -0600

    Agregando

--- app/(pages)/st/[token]/page.tsx
+++ app/(pages)/st/[token]/page.tsx
@@ -23,8 +23,8 @@ export async function generateMetadata({params} : Props): Promise<Metadata>{
             images: [
                 {
                     url: data.ImgFoto ? `https://souvenir-site.com/WebTarjet/PublicTempStorage/UsuTarjets/${data.ImgTarFrente}` : 'https://www.tarjet.site/_next/image?url=%2Fimages%2Filustracion-perfil-3.webp&w=640&q=75',
-                    width: 150,
-                    height: 250,
+                    width: 300,
+                    height: 266,
                 },
             ],
             locale: 'es_MEX',
