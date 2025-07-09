5df048997b5efb32bf49df4e41eef6cc6e128013
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2025-07-09 14:52:28 -0600

    Add: AgendarCita Page

--- app/(pages)/agendar-cita/page.tsx
+++ app/(pages)/agendar-cita/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect } from 'react';
+import { getServerSession } from "next-auth";
 import { useSession } from 'next-auth/react';
 import { redirect } from 'next/navigation';
 import styles from './page.module.scss';
