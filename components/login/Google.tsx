e637c31b75c7195d59b0af3db766d5a2b59d9478
GerardoARHernandez <gahdz14@gmail.com>
2025-07-15 09:56:14 -0600

    Update: Key de Inicio Sesión Google

--- components/login/Google.tsx
+++ components/login/Google.tsx
@@ -10,7 +10,7 @@ const Google = () => {
         // <GoogleOAuthProvider clientId="795705014478-07c6ktiul0e14v0phdibro00h17lmgh5.apps.googleusercontent.com">
         //     <BtnGoogle />
         // </GoogleOAuthProvider>
-        <GoogleOAuthProvider clientId="795705014478-07c6ktiul0e14v0phdibro00h17lmgh5.apps.googleusercontent.com.mx.mt">
+        <GoogleOAuthProvider clientId="59375683381-39l70hi4a5f2e5niajqniu6tgvtdcqri.apps.googleusercontent.com">
             <BtnGoogle />
         </GoogleOAuthProvider>
     );
